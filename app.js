04363754205fb5a9db76c2c644c019a08090adbe
jamilekarinee <139991955+jamilekarinee@users.noreply.github.com>
2024-10-10 08:34:04 -0300

    10 de outubro

--- app.js
+++ app.js
@@ -22,6 +22,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 app.use('/avaliacoes', avaliacoesRouter);                    // AVALIACOES
+app.use(express.static('public')); 
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
--- routes/avaliacoes.js
+++ routes/avaliacoes.js
@@ -3,13 +3,13 @@ var router = express.Router();
 let db = require('../utils/db');
 
 
-/* listar GENEROS */
+/* listagem */
 router.get('/listagem_privada', function (req, res) {
   let cmd = 'SELECT id_avaliacao, email_avaliacao, nome_avaliacao FROM tb_avaliacao ORDER BY id_avaliacao; ';
   db.query(cmd, [], function (erro, listagem) {
     if (erro) {
       res.send(erro);
-    }
+    } 
     res.render('avaliacoes_lista', { resultado: listagem });
   })
 });
@@ -17,19 +17,24 @@ router.get('/listagem_privada', function (req, res) {
 /* adicionar AVALIACOES */
 router.get('/add', function (req, res) { /* post vai incluir algo */
   res.render('avaliacoes', { resultado: {} });
-});
+}); 
 
 /* adicionar AVALIACOES */
 router.post('/add', function (req, res) {
   let cmd = 'INSERT INTO tb_avaliacao (email_avaliacao, nome_avaliacao) VALUES (?, ?) ';
   let email = req.body.email;
   let avaliacao_do_cliente = req.body.avaliacao_do_cliente;
-  db.query(cmd, [email, avaliacao_do_cliente], function (erro) {
-    if (erro) {
-      res.send(erro);
-    }
-    res.redirect('/avaliacoes/add');
-  })
+  if(avaliacao_do_cliente!=null && avaliacao_do_cliente.length!=0){ /* email cliente */ 
+    db.query(cmd, [email, avaliacao_do_cliente], function (erro) {
+      if (erro) {
+        res.send(erro);
+      }
+      res.redirect('/avaliacoes/add');
+    })  
+  }
+  else{
+    res.send("String não pode ser vazia"); 
+  }
 });
 
 router.delete('/delete/:id', function (req, res) {
